7c51b3e9b5318b227b9014f6df7d941b809f653a
Keenan Gray <keenangray@Keenans-MacBook-Pro-2.local>
2020-04-06 23:28:42 -0400

    initialize

---
+++ README.md
@@ -0,0 +1,5 @@
+#  Unity WebGL Server
+
+For local testing run with:
+    docker build -t golang-heroku .
+    docker run -p 3000:8080 -d golang-heroku
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	//"fmt"
+	"net/http"
+	//"time"
+	//	"log"
+	//"github.com/gorilla/mux"
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+	//"os"
+	//"fmt"
+	//"path/filepath"
+)
+
+//Go application entrypoint
+func main() {
+	var dir string
+	flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
+	flag.Parse()
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./web/public", true)))
+	//router.Use(static.Serve("/public", static.LocalFile("./web/public", true)))
+
+	// Setup route group for the API
+	api := router.Group("api/")
+	{
+		api.GET("/ping", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Server is running",
+			})
+		})
+	}
+	router.Run()
+}
