5707c96c693f565ac3ec2a6066bc862fe1781233
Keenan Gray <keenangray@keenans-mbp-2.home>
2020-04-18 21:55:03 -0400

    remove old code

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	context "context"
 	"flag"
-	"fmt"
-
-	"log"
-	"math/rand"
 
 	//"fmt"
 	"net/http"
@@ -24,25 +19,6 @@ const (
 	port       = ":8081"
 	colorBytes = 3
 )
-
-type server struct{}
-
-//Return a random color in the form of a color hex string
-//e.g. "#00FF00" would be RRGGBB so Green
-func (s *server) GetRandomColor(ctx context.Context, curr *pb.CurrentColor) (*pb.NewColor, error) {
-	hex := "#" + randomHex()
-	log.Printf("Client's current color: [#%v] sending [%v]", curr.Color, hex)
-	return &pb.NewColor{Color: hex}, nil
-}
-
-//Create a random hex string of N digits
-func randomHex() string {
-	bytes := make([]byte, colorBytes)
-	if _, err := rand.Read(bytes); err != nil {
-		log.Panicln("Error generating random hex value", err)
-	}
-	return fmt.Sprintf("%X", bytes)
-}
 
 //Go application entrypoint
 func main() {
